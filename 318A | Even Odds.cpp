1defd348683239fd9ffa63ab8aba9cbd954c22fd
Mithilesh Lathkar <119594987+MithileshL-26@users.noreply.github.com>
2022-12-10 15:24:02 +0530

    Codeforces Solution for 318A | Even Odds

---
+++ 318A | Even Odds.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#include "iostream"
+#include "vector"
+#include "map"
+#include "unordered_map"
+#include "utility"
+#include "algorithm"
+#include "math.h"
+#include "iterator"
+
+using namespace std;
+
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define PI 3.141592653589793238462
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> p;
+typedef vector< pair<int, int> > v_p;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    
+    if(k <= (n+1) / 2) // kth Number is ODD
+    {
+        cout << k * 2 - 1 << nline;
+    }
+    else // kth Number is EVEN
+    {
+        cout << (k - (n + 1) / 2) * 2 << nline;
+    }
+}
+int main()
+{	
+	fastio();
+	
+	solve();
+	
+	return 0;
+}
