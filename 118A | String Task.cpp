61edc8c81901866bd1634d0968acc25caf1304dd
Mithilesh Lathkar <119594987+MithileshL-26@users.noreply.github.com>
2023-01-03 17:10:39 +0530

    Codeforces Solution for 118A | String Task

---
+++ 118A | String Task.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+#include "iostream"
+#include "vector"
+#include "map"
+#include "unordered_map"
+#include "utility"
+#include "algorithm"
+#include "math.h"
+#include "iterator"
+
+using namespace std;
+
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define PI 3.141592653589793238462
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> p;
+typedef vector< pair<int, int> > v_p;
+
+void solve()
+{
+    string s, t;
+    cin >> s;
+    
+    transform(s.begin(), s.end(), s.begin(), :: tolower);
+  
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+        {
+            continue;
+        }
+        else
+        {
+            t += '.';
+            t += s[i]; 
+        }
+    }
+    cout << t << nline;
+}
+int main()
+{	
+	fastio();
+	
+	solve();
+	
+	return 0;
+}
